d13c94320c4a33818bbc6b277b5a20ca982d063d
burzynnn <kubatkh@gmail.com>
2019-09-03 23:32:13 +0200

    Prepare workflow to create new version

--- nodejs/controllers/404/404Controller.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const path = require("path");
-const router404 = express.Router();
-
-router404.get("*", function (req, res) {
-    res.status(404).sendFile(path.join(__dirname + "../../.." + "/public/404.html"));
-});
-
-module.exports = router404;
