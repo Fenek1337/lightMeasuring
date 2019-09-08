8db6446bfa72555cd2de4f3bf09b57aa0c8b5583
burzynnn <kubatkh@gmail.com>
2019-09-09 01:06:45 +0200

    rebuild and simplify whole file

--- nodejs/public/js/index.js
+++ nodejs/public/js/index.js
@@ -1,31 +1,14 @@
-let ctxFirst = document.getElementById("chartFirst");
-let ctxSecond = document.getElementById("chartSecond");
-let ctxThird = document.getElementById("chartThird");
-let ctxFourth = document.getElementById("chartFourth");
-let ctxFifth = document.getElementById("chartFifth");
-let ctxSixth = document.getElementById("chartSixth");
-let ctxSeventh = document.getElementById("chartSeventh");
-let ctxEighth = document.getElementById("chartEighth");
-let ctxNinth = document.getElementById("chartNinth");
-let ctxTenth = document.getElementById("chartTenth");
-let ctxEleventh = document.getElementById("chartEleventh");
-let ctxTwelfth = document.getElementById("chartTwelfth");
-let ctxThirteenth = document.getElementById("chartThirteenth");
-let ctxFourteenth = document.getElementById("chartFourteenth");
-let ctxFifteenth = document.getElementById("chartFifteenth");
-let ctxSixteenth = document.getElementById("chartSixteenth");
-
 const settings = {
     type: 'line',
     data: {
         labels: [],
         datasets: [{
             fontColor: "#ecf0f1",
-            borderColor: "#FF5733",
+            borderColor: "rgba(51, 217, 178,1.0)",
             data: [],
             fill: false,
             pointStyle: 'circle',
-            backgroundColor: '#FF5733',
+            backgroundColor: 'rgba(51, 217, 178,1.0)',
             pointRadius: 3,
             pointHoverRadius: 5,
             lineTension: 0.3,
@@ -33,6 +16,11 @@ const settings = {
     },
     options: {
         responsive: true,
+        layout: {
+            padding: {
+                right: 40
+            }
+        },
         legend: {
             display: false
         },
@@ -46,7 +34,7 @@ const settings = {
         },
         plugins: {
             datalabels: {
-                backgroundColor: '#FF5733',
+                backgroundColor: 'rgba(51, 217, 178,1.0)',
                 borderRadius: 4,
                 color: 'white',
                 font: {
@@ -73,140 +61,40 @@ const settings = {
     }
 }
 
-let chartFirst = new Chart(ctxFirst, JSON.parse(JSON.stringify(settings)));
-chartFirst.options.title.text = "Lux1";
-let chartSecond = new Chart(ctxSecond, JSON.parse(JSON.stringify(settings)));
-chartSecond.options.title.text = "Lux2";
-let chartThird = new Chart(ctxThird, JSON.parse(JSON.stringify(settings)));
-chartThird.options.title.text = "Lux3";
-let chartFourth = new Chart(ctxFourth, JSON.parse(JSON.stringify(settings)));
-chartFourth.options.title.text = "Lux4";
-let chartFifth = new Chart(ctxFifth, JSON.parse(JSON.stringify(settings)));
-chartFifth.options.title.text = "Lux5";
-let chartSixth = new Chart(ctxSixth, JSON.parse(JSON.stringify(settings)));
-chartSixth.options.title.text = "Lux6";
-let chartSeventh = new Chart(ctxSeventh, JSON.parse(JSON.stringify(settings)));
-chartSeventh.options.title.text = "Lux7";
-let chartEighth = new Chart(ctxEighth, JSON.parse(JSON.stringify(settings)));
-chartEighth.options.title.text = "Lux8";
-let chartNinth = new Chart(ctxNinth, JSON.parse(JSON.stringify(settings)));
-chartNinth.options.title.text = "Lux9";
-let chartTenth = new Chart(ctxTenth, JSON.parse(JSON.stringify(settings)));
-chartTenth.options.title.text = "Lux10";
-let chartEleventh = new Chart(ctxEleventh, JSON.parse(JSON.stringify(settings)));
-chartEleventh.options.title.text = "Lux11";
-let chartTwelfth = new Chart(ctxTwelfth, JSON.parse(JSON.stringify(settings)));
-chartTwelfth.options.title.text = "Lux12";
-let chartThirteenth = new Chart(ctxThirteenth, JSON.parse(JSON.stringify(settings)));
-chartThirteenth.options.title.text = "Lux13";
-let chartFourteenth = new Chart(ctxFourteenth, JSON.parse(JSON.stringify(settings)));
-chartFourteenth.options.title.text = "Lux14";
-let chartFifteenth = new Chart(ctxFifteenth, JSON.parse(JSON.stringify(settings)));
-chartFifteenth.options.title.text = "Lux15";
-let chartSixteenth = new Chart(ctxSixteenth, JSON.parse(JSON.stringify(settings)));
-chartSixteenth.options.title.text = "Lux16";
+const ctx = document.querySelectorAll(".canvas");
 
-let socket = io.connect("http://localhost:4000");
+let chartFirst, chartSecond, chartThird, chartFourth, chartFifth, chartSixth, chartSeventh, chartEighth, chartNinth, chartTenth, chartEleventh, chartTwelfth, chartThirteenth, chartFourteenth, chartFifteenth, chartSixteenth;
+
+const charts = [chartFirst, chartSecond, chartThird, chartFourth, chartFifth, chartSixth, chartSeventh, chartEighth, chartNinth, chartTenth, chartEleventh, chartTwelfth, chartThirteenth, chartFourteenth, chartFifteenth, chartSixteenth];
+
+for (var i = 0; i < charts.length; i++) {
+    charts[i] = new Chart(ctx[i], JSON.parse(JSON.stringify(settings)));
+    charts[i].options.title.text = `Lux${i + 1}`;
+}
+let socket = io();
 
 // TODO: zrobić funkcję, która będzie sprawdzać czy przez Socket przechodzi dany topic.
 // TODO: zrobić funkcję, która będzie zwracać średnie z godziny z pojedynczych/wszystkich czujników.
 
 let manageCharts = function (chart, msg) {
-    if (chart.data.labels.length != 15) {
+    if (chart.data.labels.length != 5) {
         chart.data.labels.push("");
         chart.data.datasets.forEach(function (dataset) {
-            dataset.data.push(msg.message);
+            dataset.data.push(msg);
         });
     } else {
         chart.data.labels.shift();
         chart.data.labels.push("");
         chart.data.datasets.forEach(function (dataset) {
             dataset.data.shift();
-            dataset.data.push(msg.message);
+            dataset.data.push(msg);
         });
     }
     chart.update();
 };
 
-socket.on("connect", function () {
-    socket.on("lux1", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux2", function (data) {
-
-        manageCharts(chartSecond, data);
-
-    });
-    socket.on("lux3", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux4", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux5", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux6", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux7", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux8", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux9", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux10", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux11", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux12", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux13", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux14", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux15", function (data) {
-
-        manageCharts(chartFirst, data);
-
-    });
-    socket.on("lux16", function (data) {
-
-        manageCharts(chartFirst, data);
-
+socket.on("connect", () => {
+    socket.on("lux", (data) => {
+        manageCharts(charts[data.sensorShort - 1], data.measurement);
     });
 });
