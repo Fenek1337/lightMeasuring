30ee21f6b41c88fc757946c8af871b08334a1561
burzynnn <kubatkh@gmail.com>
2019-09-10 21:59:22 +0200

    handle new charts, insert values on load, add support for two-page design

--- nodejs/public/js/index.js
+++ nodejs/public/js/index.js
@@ -1,3 +1,5 @@
+const socket = io();
+
 const settings = {
     type: 'line',
     data: {
@@ -61,31 +63,37 @@ const settings = {
     }
 }
 
+const settingsAdditional = settings;
+settingsAdditional.data.datasets.borderColor = "rgba(252, 92, 101,1.0)";
+settingsAdditional.data.datasets.backgroundColor = "rgba(252, 92, 101,1.0)";
+
 const ctx = document.querySelectorAll(".canvas");
 
-let chartFirst, chartSecond, chartThird, chartFourth, chartFifth, chartSixth, chartSeventh, chartEighth, chartNinth, chartTenth, chartEleventh, chartTwelfth, chartThirteenth, chartFourteenth, chartFifteenth, chartSixteenth;
+let chartFirst, chartSecond, chartThird, chartFourth, chartFifth, chartSixth, chartSeventh, chartEighth, chartNinth, chartTenth, chartEleventh, chartTwelfth, chartThirteenth, chartFourteenth, chartFifteenth, chartSixteenth, chartSeventeenth, chartEighteenth, chartNineteenth, chartTwentyth, chartTwentyFirst, chartTwentySecond;
 
-const charts = [chartFirst, chartSecond, chartThird, chartFourth, chartFifth, chartSixth, chartSeventh, chartEighth, chartNinth, chartTenth, chartEleventh, chartTwelfth, chartThirteenth, chartFourteenth, chartFifteenth, chartSixteenth];
+const charts = [chartFirst, chartSecond, chartThird, chartFourth, chartFifth, chartSixth, chartSeventh, chartEighth, chartNinth, chartTenth, chartEleventh, chartTwelfth, chartThirteenth, chartFourteenth, chartFifteenth, chartSixteenth, chartSeventeenth, chartEighteenth, chartNineteenth, chartTwentyth, chartTwentyFirst, chartTwentySecond];
 
 for (var i = 0; i < charts.length; i++) {
-    charts[i] = new Chart(ctx[i], JSON.parse(JSON.stringify(settings)));
+    if (i < 16) {
+        charts[i] = new Chart(ctx[i], JSON.parse(JSON.stringify(settings)));
+    } else {
+        charts[i] = new Chart(ctx[i], JSON.parse(JSON.stringify(settingsAdditional)));
+    }
+
     charts[i].options.title.text = `Lux${i + 1}`;
 }
-let socket = io();
 
 // TODO: zrobić funkcję, która będzie sprawdzać czy przez Socket przechodzi dany topic.
 // TODO: zrobić funkcję, która będzie zwracać średnie z godziny z pojedynczych/wszystkich czujników.
 
-let manageCharts = function (chart, msg) {
+function manageCharts(chart, msg) {
     if (chart.data.labels.length != 5) {
         chart.data.labels.push("");
-        chart.data.datasets.forEach(function (dataset) {
-            dataset.data.push(msg);
-        });
+        chart.data.datasets.forEach((dataset) => dataset.data.push(msg));
     } else {
         chart.data.labels.shift();
         chart.data.labels.push("");
-        chart.data.datasets.forEach(function (dataset) {
+        chart.data.datasets.forEach((dataset) => {
             dataset.data.shift();
             dataset.data.push(msg);
         });
@@ -93,8 +101,44 @@ let manageCharts = function (chart, msg) {
     chart.update();
 };
 
+for (let arr in queries) {
+    queries[arr].reverse();
+    for (let i = 0; i < queries[arr].length; i++) {
+        charts[arr - 1].data.labels.push("");
+        charts[arr - 1].data.datasets.forEach((dataset) => dataset.data.push(Number(queries[arr][i].value).toFixed(2)));
+        charts[arr - 1].update();
+    }
+    i = 0;
+}
+
 socket.on("connect", () => {
     socket.on("lux", (data) => {
         manageCharts(charts[data.sensorShort - 1], data.measurement);
     });
 });
+let pos = "left";
+
+const buttonLeft = document.getElementById("left");
+const buttonRight = document.getElementById("right");
+const buttonSubmit = document.getElementById("submit");
+
+buttonSubmit.addEventListener("click", () => console.log("clicked"));
+buttonLeft.addEventListener("click", () => {
+    if (pos === "right") {
+        window.scrollBy(-window.innerWidth, 0);
+        pos = "left";
+        buttonLeft.classList.remove("red");
+        buttonRight.classList.remove("red");
+        buttonSubmit.classList.remove("red");
+    }
+});
+buttonRight.addEventListener("click", () => {
+    if (pos === "left") {
+        window.scrollBy(window.innerWidth, 0);
+        pos = "right";
+        buttonLeft.classList.add("red");
+        buttonRight.classList.add("red");
+        buttonSubmit.classList.add("red");
+    }
+});
+
